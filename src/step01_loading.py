27f75adf856267fbb9c724fe10110ef3e2943903
Simon Schindler <simon@alle-schindlers.de>
2026-03-13 11:30:51 +0100

    fix relabeling of eog

--- src/step01_loading.py
+++ src/step01_loading.py
@@ -5,11 +5,19 @@ from mne.io.edf.edf import RawEDF
 def load_data(bids_path: BIDSPath) -> RawEDF:
     """Load raw EEG data from BIDS path."""
 
-    raw = read_raw_bids(bids_path)
+    raw: RawEDF = read_raw_bids(bids_path)
     raw.load_data()
 
-    # TODO: Test
-    rename_map = {f"EXG{i}": f"EOG{i}" for i in range(1, 9)}
-    raw.rename_channels(rename_map)
+    # TODO: confirm?
+    # Apperantly only EXOG4 and EXG5 are EOG channels
+    # rename_map = {"EXG4": "EOG4", "EXG5": "EOG5"}
+    # raw.rename_channels(rename_map)
+
+    for ch in ["EXG4", "EXG5"]:
+        raw.set_channel_types({ch: "eog"})
+        raw.rename_channels({ch: ch.replace('X', 'O')})
+
+    for ch in ["EXG1", "EXG2", "EXG3", "EXG6", "EXG7", "EXG8"]:
+        raw.set_channel_types({ch: "misc"})
 
     return raw
